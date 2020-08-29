a7434b57fade924154c932e96b8f67112c885d8d
Ramon Cristopher Calam <ramoncristopher.calam@g.msuiit.edu.ph>
2020-08-29 15:17:46 +0800

    Initial index webpage for Aquila.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 (function () {
   "use strict";
 
-  angular.module('Hotspot', [])
-    .controller('HotspotController', HotspotController);
+  angular
+    .module("Hotspot", [])
+    .controller("HotspotController", HotspotController);
 
-  HotspotController.$inject = ['$scope'];
+  HotspotController.$inject = ["$scope"];
   function HotspotController($scope) {
     $scope.username = $(username);
     $scope.ip = "$(ip)";
     $scope.welcome = "$(ip)";
   }
-
 })();
