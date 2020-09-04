5dfc2ef014eb7f21a1edb487eebadb9c515e8ec8
Ramon Cristopher Calam <ramoncristopher.calam@g.msuiit.edu.ph>
2020-09-04 08:21:44 +0800

    Used jQuery to focus username.

--- js/app.js
+++ js/app.js
@@ -13,10 +13,112 @@ $(function () {
 (function () {
   "use strict";
 
-  angular.module("Aquila", []).controller("AquilaController", AquilaController);
+  angular
+    .module("Aquila", [])
+    .controller("AquilaController", AquilaController)
+    //.controller("ServerVariablesController", ServerVariablesController)
+    .service("ServerVariablesService", ServerVariablesService)
+    .directive("variablesDirective", VariablesDirective)
+    .directive("focusDirective", FocusDirective);
 
-  AquilaController.$inject = ["$scope"];
-  function AquilaController($scope) {
-    $scope.brandName = "Aquila Tekno Solutions";
+  AquilaController.$inject = ["ServerVariablesService"];
+  function AquilaController(ServerVariablesService) {
+    var aqc = this;
+    aqc.brandName = "Aquila Tekno Solutions";
+
+    aqc.serverVariables = {};
+
+    var promise = ServerVariablesService.getServerVariableObject();
+    promise
+      .then(function (response) {
+        for (var variable in response.data) {
+          aqc.serverVariables[variable] = response.data[variable];
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    aqc.checkServerVariables = function () {
+      return Object.keys(aqc.serverVariables).length === 6;
+    };
+  }
+
+  ServerVariablesService.$inject = ["$http"];
+  function ServerVariablesService($http) {
+    var service = this;
+
+    service.getServerVariableObject = function () {
+      var response = $http({
+        method: "GET",
+        url: "server_variables.txt",
+        headers: {
+          "Content-type": "application/json",
+        },
+      });
+
+      return response;
+    };
+  }
+
+  //ServerVariablesController.$inject = ["ServerVariablesService"];
+  //function ServerVariablesController(ServerVariablesService) {
+  //var servar = this;
+
+  //servar.checkServerVariables = function () {
+  //if (servar.serverVariables) {
+  //return true;
+  //}
+  //return false;
+  //};
+  //}
+
+  function VariablesDirective() {
+    var ddo = {
+      controller: AquilaController,
+      controllerAs: "aqc",
+      bindToController: true,
+      restrict: "A",
+      scope: {
+        serverVariables: "=",
+      },
+      link: VariablesDirectiveLink,
+    };
+
+    return ddo;
+  }
+
+  function VariablesDirectiveLink(scope, element, attrs, controller) {
+    scope.$watch("aqc.checkServerVariables()", function (newValue, oldValue) {
+      if (newValue === true) {
+        var serverVariables = controller.serverVariables;
+        for (var category in serverVariables) {
+          //console.log("<!--" + category + "-->");
+          for (var variable in serverVariables[category]) {
+            var variableValue = element.find("#" + variable).text();
+            serverVariables[category][variable] = variableValue;
+            //console.log(variable + ": " + variableValue);
+          }
+        }
+      }
+    });
+
+    //var chapID = element.find("#chap-id").text();
+    //var chapChallenge = element.find("#chap-challenge").text();
+    //console.log(scope.$parent.aqc);
+    //console.log(chapID, chapChallenge);
+  }
+
+  function FocusDirective() {
+    var ddo = {
+      link: FocusDirectiveLink,
+    };
+
+    return ddo;
+  }
+
+  function FocusDirectiveLink(scope, element, attrs, controller) {
+    var username = element.find("input");
+    username.focus();
   }
 })();
